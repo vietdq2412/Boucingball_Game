730fd719f94c2edcbe24b0a732a6794de6d8d2e3
viet <vietdq2412@gmail.com>
2020-09-04 09:50:13 +0700

    update 4/9

---
+++ js/class.js
@@ -0,0 +1,152 @@
+let score=[0,0];
+const X = 800;
+const Y = 400;
+let fps = 100;
+
+
+function GameBoard() {
+    this.width = X;
+    this.height = Y;
+
+    //his.score=[0,0];
+    this.ball = [];
+    this.bar = [];
+    this.isOver = false;
+
+    this.getBall = function (b) {
+        this.ball.push(b);
+    }
+    this.getBar = function (bar) {
+        this.bar.push(bar);
+    }
+
+    this.drawBoard = function () {
+        let board = '<canvas id="myCanvas" width="' + this.width + '" height="' + this.height + '"></canvas>';
+        return board;
+    }
+
+    this.drawBar = function (bar) {
+        let index = this.bar.indexOf(bar);
+        context.beginPath();
+        context.rect(this.bar[index].coorX, this.bar[index].coorY, this.bar[index].width, this.bar[index].height);
+        context.fillStyle = 'red';
+        context.fill();
+        context.closePath();
+    }
+
+    this.drawBall = function (b) {
+        let color = getRandomColor();
+        let index = this.ball.indexOf(b);
+        context.beginPath();
+        context.arc(this.ball[index].coorX, this.ball[index].coorY, this.ball[index].radius, 0, 2 * Math.PI);
+        context.fillStyle = color;
+        context.fill();
+        context.closePath();
+    }
+
+    this.clear = function () {
+        context.clearRect(0, 0, this.width, this.height);
+    }
+
+    this.reset = function () {
+    }
+}
+
+/////////////////////////////////////////////////////////////////
+function Ball(name) {
+    this.name = name;
+    this.coorX = X / 2;
+    this.coorY = Y / 2;
+    this.radius = 20;
+    this.defaultSpeedX = Math.random()*7 + 3;
+    this.defaultSpeedY = Math.random()*7 + 3;
+
+    this.speedX = this.defaultSpeedX;
+    this.speedY = this.defaultSpeedY;
+
+
+    this.angle;
+
+    this.move = function () {
+        this.coorX += this.speedX;
+        this.coorY += this.speedY;
+
+        if (this.coorX + this.radius > canvas.width && this.speedX > 0) {
+            this.speedX = -this.speedX;
+        }
+
+        if (this.coorY - this.radius < 0 && this.speedY < 0) {
+            this.speedY = -this.speedY;
+            fps -= 2;
+        }
+        if (this.coorY + this.radius > canvas.height && this.speedY > 0) {
+            this.speedY = -this.speedY;
+            fps -= 2;
+        }
+    };
+
+    this.bounce = function (bar) {
+        console.log(bar.width + ' - ' + this.radius);
+        let c = false;
+        if (bar.name == 'barLeft') {
+            if (this.coorY + this.radius / 2 >= bar.coorY &&
+                this.coorY - this.radius / 2 <= bar.coorY + bar.height &&
+                this.coorX - this.radius - bar.width < bar.coorX &&
+                this.coorX - this.radius > bar.coorX &&
+                this.speedX < 0) {
+                c = true;
+            }
+        } else {
+            if (this.coorY + this.radius / 2 >= bar.coorY &&
+                this.coorY - this.radius / 2 <= bar.coorY + bar.height &&
+                this.coorX + this.radius > bar.coorX &&
+                //this.coorX + this.radius + bar.width < bar.coorX + bar.width &&
+                this.speedX > 0) {
+                c = true;
+            }
+        }
+        if (c) {
+            this.speedX = -(this.speedX * 6 / 5);
+            this.speedY = this.speedY * 6 / 5;
+            bar.height -= 8;
+            if (bar.height < 22) bar.height = 22;
+            console.log('bounce');
+        }
+    };
+}
+
+////////////////////////////////////////////////////////
+function Bar(coordinateX, name) {
+    this.name = name;
+    this.width = 10;
+    this.height = 150;
+    this.coorX = coordinateX;
+    this.coorY = canvas.height / 2 - this.height / 2;
+    this.speed = 20;
+
+    this.isMovingUp = false;
+    this.isMovingDown = false;
+
+    this.drawBar = function () {
+        context.beginPath();
+        context.rect(this.coorX, this.coorY)
+        context.endPath();
+    }
+
+    this.moveUp = function () {
+        this.coorY -= this.speed;
+    }
+
+    this.moveDown = function () {
+        this.coorY += this.speed;
+    }
+
+    this.barBound = function () {
+        if (this.coorY < 0) {
+            this.coorY = 0;
+        }
+        if (this.coorY + this.height > Y) {
+            this.coorY = Y - this.height;
+        }
+    };
+}
---
+++ js/main.js
@@ -0,0 +1,167 @@
+let gameBoard = new GameBoard();
+let data = gameBoard.drawBoard();
+document.getElementById('board').innerHTML = data;
+
+let canvas = document.getElementById('myCanvas');
+let context = canvas.getContext('2d');
+canvas.width = X;
+canvas.height = Y;
+
+let paddle = new Bar(70, 'barLeft');
+let paddle2 = new Bar(canvas.width - 70, 'barRight');
+gameBoard.getBar(paddle);
+gameBoard.getBar(paddle2);
+
+
+let ball = new Ball('b1');
+gameBoard.getBall(ball);
+randomDirection(ball);
+//gameBoard.drawBall(ball);
+let ball2 = new Ball('b2');
+gameBoard.getBall(ball2);
+randomDirection(ball2);
+
+function randomDirection(b) {
+    let random = Math.random() * 2 < 1;
+    if (random) {
+        b.speedX = -b.speedX;
+    }
+    if (Math.random() * 2 < 1) {
+        b.speedY = -b.speedY;
+        console.log()
+    }
+}
+
+
+run(gameBoard);
+////
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode == 83) {
+        paddle.isMovingDown = true;
+    }
+    if (event.keyCode == 87) {
+        paddle.isMovingUp = true;
+    }
+    console.log(event);
+});
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode == 83) {
+        paddle.isMovingDown = false;
+
+    }
+    if (event.keyCode == 87) {
+        paddle.isMovingUp = false;
+    }
+});
+
+////
+document.addEventListener('keydown', function (event) {
+
+    if (event.keyCode == 40) {
+        paddle2.isMovingDown = true;
+    }
+    if (event.keyCode == 38) {
+        paddle2.isMovingUp = true;
+    }
+});
+document.addEventListener('keyup', function (event) {
+
+    if (event.keyCode == 40) {
+        paddle2.isMovingDown = false;
+
+    }
+    if (event.keyCode == 38) {
+        paddle2.isMovingUp = false;
+    }
+});
+
+///////////////////
+
+
+function run(game) {
+    ///////////////////////
+    let isCotinue = true;
+    if (!game.isOver) {
+        game.clear();
+
+        for (let i = 0; i < game.ball.length; i++) {
+            game.ball[i].bounce(paddle);
+            game.ball[i].bounce(paddle2);
+            game.ball[i].move();
+
+            if (game.ball[i].coorX < game.ball[i].radius && game.ball[i].speedX < 0) {
+                //game.ball[i].coorX = 20;
+                game.isOver = true;
+                score[1] = score[1] + 1;
+                document.getElementById('1').innerHTML = score[1];
+                console.log(score);
+
+            }
+            if (game.ball[i].coorX + game.ball[i].radius >= canvas.width) {
+                game.isOver = true;
+                console.log('right');
+                score[0] = score[0] + 1;
+                document.getElementById('0').innerHTML = score[0];
+                console.log(score);
+
+            }
+
+            game.drawBall(game.ball[i]);
+        }
+
+        for (let i = 0; i < game.bar.length; i++) {
+            if (game.bar[i].isMovingDown) {
+                game.bar[i].moveDown();
+            }
+            if (game.bar[i].isMovingUp) {
+                game.bar[i].moveUp();
+            }
+            game.bar[i].barBound();
+            game.drawBar(game.bar[i]);
+        }
+    } else {
+        let check = confirm('play again? \n' + 'score: ' + score);
+        if (check) {
+            for (let i = 0; i < game.ball.length; i++) {
+                game.ball[i].coorX = canvas.width / 2;
+                game.ball[i].coorY = canvas.height / 2;
+
+                randomDirection(game.ball[i]);
+
+                game.ball[i].speedX = game.ball[i].defaultSpeedX;
+                game.ball[i].speedY = game.ball[i].defaultSpeedY;
+            }
+            for (let i = 0; i < game.bar.length; i++) {
+                game.bar[i].height = 150;
+            }
+
+            game.isOver = false;
+            isCotinue = true;
+        } else {
+            game.isOver = true;
+            isCotinue = false;
+        }
+    }
+    if (isCotinue) {
+        setTimeout(() => run(game), 30);
+    } else {
+        alert('good bye!');
+
+        window.location.href = 'index.html';
+    }
+
+    //requestAnimationFrame(run);
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+function getRandomHex(assizes) {
+    return Math.floor(Math.random() * assizes);
+}
+
+function getRandomColor() {
+    let red = getRandomHex(255);
+    let green = getRandomHex(255);
+    let blue = getRandomHex(255);
+    return "rgb(" + red + "," + green + "," + blue + ")";
+}
